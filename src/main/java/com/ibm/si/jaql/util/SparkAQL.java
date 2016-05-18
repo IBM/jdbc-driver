3e15b829d594ebb000b6c403770e4589b0fe9174
Ian Molloy <molloyim@us.ibm.com>
2016-05-18 15:33:23 -0400

    Fixed a bug where the most basic case (you provide Spark with only the table name) failed to be modified appropriately.

--- src/main/java/com/ibm/si/jaql/util/SparkAQL.java
+++ src/main/java/com/ibm/si/jaql/util/SparkAQL.java
@@ -19,6 +19,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 public class SparkAQL {
 	private static Pattern getSchemaPattern = Pattern.compile("^SELECT\\s+\\*\\s+FROM\\s+\\((SELECT\\s+(.*)\\s+FROM\\s+(\\S+)\\s+WHERE\\s+(.*))\\)\\s+WHERE\\s+1=0$", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
 	private static Pattern selectStarPattern = Pattern.compile("^SELECT\\s+(.*?)\\s+FROM\\s+\\((SELECT\\s+(.*)\\s+FROM\\s+(\\S+)\\s+WHERE\\s+(.*))\\)\\s*$", Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+	private static Pattern removeWherePattern = Pattern.compile("(.*)\\s*WHERE\\s+1\\s*=\\s*0\\s*$");
 
 	static final Logger logger = LogManager.getLogger(SparkAQL.class.getName());
 
@@ -70,61 +71,62 @@ public class SparkAQL {
 		}
 	}
 	
+	private static String getSchemaCoreQuery(final String query)
+	{
+		Matcher match = getSchemaPattern.matcher(query);
+		if (match.matches()) return match.group(1);
+		match = removeWherePattern.matcher(query);
+		if (match.matches()) return match.group(1);
+		logger.fatal("Failed to parse and convert an SQL to AQL query. Should not get here.");
+		return query;
+	}
 	/**
 	 * Reformats the query to insert a LIMIT 1 prior to a Time constraint
 	 */
 	private static String getSchemaQuery(final String query)
 	{
-		Matcher match = getSchemaPattern.matcher(query);
-		if (match.matches())
+		String stmt = getSchemaCoreQuery(query);
+		final AQLParser aqlParser = 
+				new AQLParser(new CommonTokenStream(new AQLLexer(new ANTLRInputStream(stmt))));
+		final StatementContext statement = aqlParser.statement();
+		if (AQLParser.SELECT != statement.start.getType())
 		{
-			String stmt = match.group(1);
-			final AQLParser aqlParser = 
-					new AQLParser(new CommonTokenStream(new AQLLexer(new ANTLRInputStream(stmt))));
-			final StatementContext statement = aqlParser.statement();
-			if (AQLParser.SELECT != statement.start.getType())
-			{
-				//what do we want to do if it the AQL query is non select ? thrown an exception ?
-				return query;
-			}
-			int count = statement.getChildCount();
-			ParseTree queryTree = statement.getChild(0);
-			StringBuilder newQuery = new StringBuilder();
-			boolean hasLimit = false;
-			for (int i = 0; i < queryTree.getChildCount(); )
-			{
-				if (queryTree.getChild(i).getPayload() instanceof org.antlr.v4.runtime.CommonToken) {
-					if (((org.antlr.v4.runtime.CommonToken)queryTree.getChild(i).getPayload()).getType() == AQLParser.LIMIT) {
-						newQuery.append(" LIMIT 1");
-						hasLimit = true;
-						i += 2;
-						continue;
-					} else {
-						if (newQuery.length() > 0)
-							newQuery.append(' ');
-						logger.debug(queryTree.getChild(i).getPayload() + "\t" + queryTree.getChild(i).getPayload().getClass() + "\t" + queryTree.getChild(i).getText());
-						newQuery.append(queryTree.getChild(i).getText());
-						i++;
-						continue;
-					}
-				} else if (queryTree.getChild(i).getPayload() instanceof com.ibm.si.jaql.aql.AQLParser.QueryTimeContext) {
-					if (!hasLimit) {
-						newQuery.append(" LIMIT 1");
-						hasLimit = true;
-					}
-					joinParseTree(queryTree.getChild(i), newQuery, ' ');
-					i++;
-				} else {
-					joinParseTree(queryTree.getChild(i), newQuery, ' ');
-					i++;
-				}
-			}
-			return newQuery.toString();
-		} else {
-			// Error; shouldn't ever get here
-			logger.fatal("Failed to parse and convert an SQL to AQL query. Should not get here.");
+			//what do we want to do if it the AQL query is non select ? thrown an exception ?
 			return query;
 		}
+		int count = statement.getChildCount();
+		ParseTree queryTree = statement.getChild(0);
+		StringBuilder newQuery = new StringBuilder();
+		boolean hasLimit = false;
+		for (int i = 0; i < queryTree.getChildCount(); )
+		{
+			if (queryTree.getChild(i).getPayload() instanceof org.antlr.v4.runtime.CommonToken) {
+				if (((org.antlr.v4.runtime.CommonToken)queryTree.getChild(i).getPayload()).getType() == AQLParser.LIMIT) {
+					newQuery.append(" LIMIT 1");
+					hasLimit = true;
+					i += 2;
+					continue;
+				} else {
+					if (newQuery.length() > 0)
+						newQuery.append(' ');
+					logger.debug(queryTree.getChild(i).getPayload() + "\t" + queryTree.getChild(i).getPayload().getClass() + "\t" + queryTree.getChild(i).getText());
+					newQuery.append(queryTree.getChild(i).getText());
+					i++;
+					continue;
+				}
+			} else if (queryTree.getChild(i).getPayload() instanceof com.ibm.si.jaql.aql.AQLParser.QueryTimeContext) {
+				if (!hasLimit) {
+					newQuery.append(" LIMIT 1");
+					hasLimit = true;
+				}
+				joinParseTree(queryTree.getChild(i), newQuery, ' ');
+				i++;
+			} else {
+				joinParseTree(queryTree.getChild(i), newQuery, ' ');
+				i++;
+			}
+		}
+		return newQuery.toString();
 	}
 	public static void main(String[] args)
 	{
